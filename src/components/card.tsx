6226daa901e5102a57bdc4af6ab989b4654872aa
MintarasGrinius <74651345+MintarasGrinius@users.noreply.github.com>
2022-11-24 20:58:51 +0200

    Card hover box shadow

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,14 @@
 //@flow
 import * as React from 'react'
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
   makeStyles,
+  Modal,
   Typography,
 } from '@material-ui/core'
 import { generateDisplayableImage } from '../utils'
@@ -46,6 +48,21 @@ const useStyles = makeStyles({
     boxShadow:
       'rgb(0 0 0 / 10%) 0rem 0.25rem 0.375rem -0.0625rem, rgb(0 0 0 / 6%) 0rem 0.125rem 0.25rem -0.0625rem',
     overflow: 'visible',
+    transition: 'all 0.5s',
+    '& img': {
+      transition: 'all 0.5s',
+    },
+    '&:hover': {
+      transition: 'all 0.5s',
+      boxShadow:
+        'rgb(0 0 0 / 30%) 0rem 0.25rem 1.375rem -0.0625rem, rgb(0 0 0 / 6%) 0rem 0.125rem 0.25rem -0.0625rem',
+      '& img': {
+        width: '290px',
+        bottom: '1.3rem',
+        height: '210px',
+        transition: 'all 0.5s',
+      },
+    },
   },
   media: {
     width: '280px',
@@ -100,6 +117,20 @@ const useStyles = makeStyles({
     fontSize: '14px',
     margin: '1.2px 4px 0px 0px',
   },
+  modalBax: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '400px',
+    backgroundColor: 'rgb(18, 18, 18)',
+    border: '2px solid rgb(0, 0, 0)',
+    boxShadow:
+      'rgb(0 0 0 / 20%) 0px 11px 15px -7px, rgb(0 0 0 / 14%) 0px 24px 38px 3px, rgb(0 0 0 / 12%) 0px 9px 46px 8px',
+    padding: '32px',
+    color: '#fff',
+    borderRadius: '12px',
+  },
 })
 
 const EventCard = ({
@@ -120,27 +151,44 @@ const EventCard = ({
 }: Props) => {
   const classes = useStyles()
   const data = new Date(time)
+  const [open, setOpen] = React.useState(false)
+
   return (
-    <div className={classes.container}>
-      <CardMedia
-        classes={{ root: classes.media }}
-        component="img"
-        height="140"
-        image={generateDisplayableImage(photos?.[0])}
-        alt="green iguana"
-      />
-      <div className={classes.contentContainer}>
-        <div className={classes.title}>{title}</div>
-        <div className={classes.subtitle}>{description}</div>
-        <div className={classes.time}>
-          <div>
-            <AccessTimeIcon classes={{ root: classes.icon }} />
-            {data.toDateString()}
+    <>
+      <div onClick={() => setOpen(true)} className={classes.container}>
+        <CardMedia
+          classes={{ root: classes.media }}
+          component="img"
+          height="140"
+          image={generateDisplayableImage(photos?.[0])}
+          alt="green iguana"
+        />
+        <div className={classes.contentContainer}>
+          <div className={classes.title}>{title}</div>
+          <div className={classes.subtitle}>{description}</div>
+          <div className={classes.time}>
+            <div>
+              <AccessTimeIcon classes={{ root: classes.icon }} />
+              {data.toDateString()}
+            </div>
+            <div>{data.toLocaleTimeString()}</div>
           </div>
-          <div>{data.toLocaleTimeString()}</div>
         </div>
       </div>
-    </div>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={classes.modalBax}>
+          <Typography id="modal-modal-title">Text in a modal</Typography>
+          <Typography id="modal-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </>
   )
 }
 
