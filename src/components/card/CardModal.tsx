36ee379b6f3e8db3877b400cfb308a96372709a3
MintarasGrinius <74651345+MintarasGrinius@users.noreply.github.com>
2022-11-29 20:48:03 +0200

    Dislike

--- src/components/card/CardModal.tsx
+++ src/components/card/CardModal.tsx
@@ -16,6 +16,7 @@ import Close from '@material-ui/icons/Close'
 import { authHeaders, backURL } from '../user/env'
 import axios from 'axios'
 import classNames from 'classnames'
+import ThumbDown from '@material-ui/icons/ThumbDown'
 
 interface Props {
   event: PlannedEvent
@@ -26,7 +27,6 @@ interface Props {
 
 const useStyles = makeStyles({
   modalBax: {
-    overflow: 'hidden',
     position: 'absolute',
     top: '50%',
     left: '50%',
@@ -49,6 +49,11 @@ const useStyles = makeStyles({
     },
   },
   imageContainer: {
+    overflow: 'hidden',
+    borderRadius: '0.75rem 0.75rem 0 0',
+  },
+  image: {
+    overflow: 'hidden',
     height: '50%',
     position: 'relative',
     minHeight: '250px',
@@ -103,17 +108,16 @@ const useStyles = makeStyles({
     left: '50%',
     transform: 'translate(calc(-50% + 170px), calc(-50% + 240px))',
     width: 'max-content',
-    borderRadius: '0.75rem',
-    border: 'none',
-    color: 'rgb(255, 255, 255)',
-    padding: '0.5rem 1rem',
     zIndex: 10000,
+    display: 'flex',
+    '& button': {
+      cursor: 'pointer',
+    },
   },
   apply: {
     background: 'linear-gradient(195deg, rgb(236, 64, 122), rgb(216, 27, 96))',
     boxShadow:
       'rgb(0 0 0 / 14%) 0rem 0.25rem 1.25rem 0rem, rgb(233 30 98 / 40%) 0rem 0.4375rem 0.625rem -0.3125rem',
-    cursor: 'pointer',
     transition: 'all 0.2s ease-in-out',
     '&:hover': {
       boxShadow:
@@ -122,6 +126,13 @@ const useStyles = makeStyles({
       fontSize: '1.02rem',
       transition: 'all 0.2s ease-in-out',
     },
+  },
+  badge: {
+    border: 'none',
+    padding: '0.52rem 1.02rem',
+    borderRadius: '0.75rem',
+    color: 'rgb(255, 255, 255)',
+    marginLeft: '0.5rem',
   },
   applied: {
     background: 'linear-gradient(195deg, rgb(106 106 106), rgb(64 64 64))',
@@ -155,82 +166,96 @@ const CardModal = ({
 
   return (
     <>
-      {open && !liked && (
-        <button
-          onClick={applyToAttend}
-          className={classNames(classes.apply, classes.badgeLocation)}
-        >
-          {'I wish to attend this event!'}
-        </button>
-      )}
-      {open && liked && (
-        <Badge className={classNames(classes.badgeLocation, classes.applied)}>
-          {'Applied'}
-        </Badge>
-      )}
       <Modal
         open={open}
         onClose={() => setOpen(false)}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box className={classes.modalBax}>
-          <Swiper
-            spaceBetween={50}
-            slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            modules={[Navigation]}
-            navigation
-            pagination={{ clickable: true }}
-          >
-            {photos?.map((photo) => (
-              <SwiperSlide className={classes.imageContainer}>
-                <Image
-                  src={generateDisplayableImage(photo)}
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition={'center'}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <div className={classes.contentContainer}>
-            <div>
-              <div className={classes.titleContainer}>
-                <div className={classes.title}>{title}</div>
-                <div className={classes.locationAndTimeContainer}>
-                  <div>
-                    <LocationOn fontSize={'inherit'} /> {location}
-                  </div>
-                  <div>
-                    <AccessTime fontSize={'inherit'} />
-                    {data.toDateString()}
-                    <div className={classes.time}>
-                      {data.toLocaleTimeString().slice(0, 5)}
+        <div>
+          <Box className={classes.modalBax}>
+            {open && (
+              <div className={classes.badgeLocation}>
+                {!liked ? (
+                  <>
+                    <button
+                      className={classNames(classes.badge, classes.applied)}
+                    >
+                      <ThumbDown />
+                    </button>
+                    <button
+                      onClick={applyToAttend}
+                      className={classNames(classes.apply, classes.badge)}
+                    >
+                      {'I wish to attend this event!'}
+                    </button>
+                  </>
+                ) : (
+                  <Badge className={classNames(classes.badge, classes.applied)}>
+                    {'Applied'}
+                  </Badge>
+                )}
+              </div>
+            )}
+            <div className={classes.imageContainer}>
+              <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                modules={[Navigation]}
+                navigation
+                pagination={{ clickable: true }}
+              >
+                {photos?.map((photo) => (
+                  <SwiperSlide className={classes.image}>
+                    <Image
+                      src={generateDisplayableImage(photo)}
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition={'center'}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+            <div className={classes.contentContainer}>
+              <div>
+                <div className={classes.titleContainer}>
+                  <div className={classes.title}>{title}</div>
+                  <div className={classes.locationAndTimeContainer}>
+                    <div>
+                      <LocationOn fontSize={'inherit'} /> {location}
+                    </div>
+                    <div>
+                      <AccessTime fontSize={'inherit'} />
+                      {data.toDateString()}
+                      <div className={classes.time}>
+                        {data.toLocaleTimeString().slice(0, 5)}
+                      </div>
                     </div>
                   </div>
                 </div>
+                <div className={classes.description}>{description}</div>
               </div>
-              <div className={classes.description}>{description}</div>
-            </div>
-            <div className={classes.bottomDescription}>
-              <div className={classes.doubleDetailsContainer}>
-                <div className={classes.age}>
-                  {`Age allowed from ${age_group.from} to ${age_group.to}`}
+              <div className={classes.bottomDescription}>
+                <div className={classes.doubleDetailsContainer}>
+                  <div className={classes.age}>
+                    {`Age allowed from ${age_group.from} to ${age_group.to}`}
+                  </div>
+                  <div>{`Capacity: ${capacity}`}</div>
                 </div>
-                <div>{`Capacity: ${capacity}`}</div>
-              </div>
-              <div className={classes.doubleDetailsContainer}>
-                <div>{`Party category: ${category}`}</div>
-                <div>{`Own drinks are ${
-                  own_drinks ? '' : 'not'
-                } allowed.`}</div>
-                <div>{`Dress Code: ${dress_code}`}</div>
+                <div className={classes.doubleDetailsContainer}>
+                  <div>{`Party category: ${category}`}</div>
+                  <div>{`Own drinks are ${
+                    own_drinks ? '' : 'not'
+                  } allowed.`}</div>
+                  <div>{`Dress Code: ${dress_code}`}</div>
+                </div>
               </div>
             </div>
-          </div>
-        </Box>
+          </Box>
+        </div>
       </Modal>
     </>
   )
